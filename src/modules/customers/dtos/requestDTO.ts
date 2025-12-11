def502fd9ee8221152aa480f10fb7d508e07be85
don44112000 <don44112000@gmail.com>
2025-12-11 16:50:30 +0530

    phone email rule

--- src/modules/customers/dtos/requestDTO.ts
+++ src/modules/customers/dtos/requestDTO.ts
@@ -1,4 +1,4 @@
-import { IsString, IsEmail, IsOptional, IsBoolean, MaxLength, IsNotEmpty } from 'class-validator';
+import { IsString, IsEmail, IsOptional, MaxLength, IsNotEmpty, ValidateIf } from 'class-validator';
 
 export class createCustomerDTO {
   @IsOptional()
@@ -6,17 +6,27 @@ export class createCustomerDTO {
   @MaxLength(150)
   name?: string;
 
-  @IsOptional()
+  // Email is required if phone/countryCode are not provided
+  @ValidateIf((o) => !o.phone || !o.countryCode)
+  @IsNotEmpty({ message: 'email is required when phone and countryCode are not provided' })
   @IsEmail()
   @MaxLength(255)
   email?: string;
 
-  @IsOptional()
+  // CountryCode is required if phone is provided OR if email is not provided
+  @ValidateIf((o) => o.phone || !o.email)
+  @IsNotEmpty({
+    message: 'countryCode is required when phone is provided or when email is not provided',
+  })
   @IsString()
   @MaxLength(10)
   countryCode?: string;
 
-  @IsOptional()
+  // Phone is required if countryCode is provided OR if email is not provided
+  @ValidateIf((o) => o.countryCode || !o.email)
+  @IsNotEmpty({
+    message: 'phone is required when countryCode is provided or when email is not provided',
+  })
   @IsString()
   @MaxLength(20)
   phone?: string;
